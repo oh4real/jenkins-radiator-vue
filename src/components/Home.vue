9efbfee2a617d61406524965b969ffd4f7f1761e
david byrd <david.byrd@bancvue.com>
2017-11-13 09:04:34 -0600

    renaming Hello to Home

---
+++ src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <jenkins-menu></jenkins-menu>
+</template>
+
+<script>
+import JenkinsMenu from '@/components/JenkinsMenu'
+export default {
+  name: 'home',
+  components: {
+    JenkinsMenu
+  }
+}
+</script>
+
+<style></style>
